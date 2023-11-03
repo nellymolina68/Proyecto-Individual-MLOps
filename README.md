50753e30194f423f5540167aee932b783a34ce2e
nellymolina68 <nellychepina68@gmail.com>
2023-11-02 23:07:05 -0600

    Prueba Api

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Proyecto-Individual-MLOps
+# MACHINE LEARNING OPERATIONS (MOLps)
+
+## Proyecto de Recomendación a la empresa STEAM : Plataforma multinacional de Videojuegos .
+
+
+## Objetivos del Proyecto: 
+
+ El objetivo principal del proyecto es crear un sistema de recomendación de videojuegos para usuarios en la plataforma Steam y desarrollar un MVP (Minimum Viable Product) que consiste en una API , que pueda ser consumida desde cualquier dispositivo conectado a internet. Este sistema de recomendación debe ser capaz de proporcionar a los usuarios recomendaciones personalizadas de videojuegos basadas en sus preferencias y comportamiento de compra, además de alojar y mantener esta API en la plataforma Render para garantizar su disponibilidad y escalabilidad.  
+
+#### **Pasos que involucran el proyecto desde el inicio del proceso ETL (Extract, Transform, Load) hasta la creación de la API y disponibilidad en la plataforma Render** 
+
+**1.- Importación de las librerias necesarias para trabajar en vsc con Python.**  
+
+  
+**2.- Proceso del ETL:** (Extraer, transformar, cargar)
+Se importaron los archivos y se extrajeron datos de tres archivos json comprimidos en formato zip, dos de los cuales tenían columnas anidadas, y se convirtieron en DataFrames,  utilizando la librería Pandas en Python.. Se hace la revision de la data para analizar su estructura con las funciones que nos proporciona esta librería, como:head, shape, info, describe. Isnull, etc. 
+Se realizaron tareas de validación y limpieza de datos, como eliminar duplicados, eliminar valores nulos, columnas no necesarias para nuestro modelo, cambio de tipo de datos, entre otros,  según las necesidades del proyecto. 
+Se combinaron los datos de los diferentes DataFrames, se realizaron  uniones utilizando las funciones de Pandas pd.merge().Los DataFrames resultantes se almacenaron  en una ubicación específica y se utilizaron para el análisis posterior, según los requisitos del proyecto, como son el Modelo de ML y el EDA. 
+
+### FUENTES DE DATOS: 
+
+**-Dataset** : el cual incluye tres archivos que debemos importar y luego procesar (movies_dataset.csv y credits.csv)
+**- Diccionario de datos** : con algunas descripciones de las columnas que están disponibles en el dataset. [Fuente de datos]()
+
+
+**-EDA:** Conjunto de técnicas estadísticas dirigidas a explorar, describir y resumir la información que contienen los datos, maximizando su comprensión. Con este análisis trate de comprender la relación que existe entre las varialbes del dataset, buscando outliers, creando una matriz de correlación para un mejor análisis de los datos. También haremos un análisis estadístico así como la visualización de diferentes gráficos.
+
+![Texto alternativo] (URL de la imagen)
+
+### REPOSITORIO:
+En github, creamos un repositorio donde tendremos todo los archivos de nuestro proyecto, el cual se conectará con Render con el servicio Web que creamos.
+
+### DEPLOY:
+Creamos un deploy en render para poder tener un entorno donde nuestra API, pueda ser consumida por cualquier persona.
+
+### DESARRLLO DE LA API:
+Utilizando el framework FastAPI, las consultas propuestas son:
+
+####  def PlayTimeGenre(genero):
+####  def UserForGenre(genero):
+#### def UsersRecommend(year: int):
+#### def UsersNotRecommend(year: int):
+#### def sentiment_analysis(year: int):  
+
+
+### Dejo el link a la api: [Desarrollo de la Api](https://fastapi-j9ta.onrender.com/docs)
+
+### SISTEMA DE RECOMENDACIÓN: 
+Por último crearemos un modelo de Machine Learning para la recomendación de juegos bqsándose en juegos similares. Todo esto lo llevaremos con una función a ser consumida por la API. La consulta será:
+####  **- ddef recomendacion_juego(item_name):**
+
+#### He realizado un vídeo con una breve descripción del proyecto [Video]()
+
+
---
+++ main.py
@@ -0,0 +1,177 @@
+import pandas as pd
+from fastapi import FastAPI
+import uvicorn 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+app = FastAPI(
+    title='Proyecto MLOps',
+    description='Creación Api',
+    swagger_ui_parameters={"tryItOutEnabled": True}
+)
+
+df_final_f1 = None
+df_final_f2 = None
+df_final_f3_4 = None
+df_final_f5 = None
+# df_final_f6 = None
+@app.on_event("startup")
+async def load_data():
+    global df_final_f1
+    global df_final_f2
+    global df_final_f3_4
+    global df_final_f5
+    # global df_final_f6
+    # Leer el archivo csv desde la ruta relativa y crear el dataframe global
+    df_fc1 = pd.read_csv('funcion_1.csv')
+    df_final_f1 = pd.DataFrame(df_fc1)
+    df_fc2 = pd.read_csv('funcion_2.csv')
+    df_final_f2 = pd.DataFrame(df_fc2)
+    df_fc3 = pd.read_csv('funcion_3_4.csv')
+    df_final_f3_4 = pd.DataFrame(df_fc3)
+    df_fc5 = pd.read_csv('funcion_5.csv')
+    df_final_f5 = pd.DataFrame(df_fc5)
+    # df_fc6 = pd.read_csv('funcion_6.csv')
+    # df_final_f6 = pd.DataFrame(df_fc6)
+
+print(df_final_f1)
+@app.get("/")
+async def index():
+    return ('Construyendo mi Api')
+
+@app.get("/about")
+async def about():
+    return ('Mi primera Api')
+# Funcion 1
+@app.get("/playtime/{genero}")
+async def get_playtime(genero: str):
+    # Convertir el género a minúsculas
+    genero = genero.lower()
+    
+    # Convertir los géneros del dataframe a minúsculas
+    df_final_f1['genres'] = df_final_f1['genres'].str.lower()
+    
+    # Verificar si el género existe en el dataframe
+    if genero not in df_final_f1['genres'].unique():
+        return "No se encontró el género solicitado"
+    
+    # Filtrar el dataframe por el género especificado
+    df = df_final_f1[df_final_f1['genres'] == genero]
+    
+    # Encontrar el año con más horas jugadas
+    max_year = df.loc[df['Hours_Played'].idxmax(), 'release_year']
+    
+    return {"Año de lanzamiento con más horas jugadas para Género " + genero : int(max_year)}
+# Funcion 2
+@app.get("/UserForGenre/{genero}")
+async def get_UserForGenre(genero: str):
+      # Filtrar el DataFrame por el género especificado
+    df_genero = df_final_f2[df_final_f2['genres'] == genero]
+
+    if df_genero.empty:
+        return "No se encuentró el género solicitado."
+
+    # Obtener el usuario con más horas jugadas para ese género
+    usuario_mas_horas = df_genero[df_genero['Hours_Played'] == df_genero['Hours_Played'].max()]['user_id'].values[0]
+
+    # Agrupar por año y sumar las horas jugadas
+    horas_por_anio = df_genero.groupby('release_year')['Hours_Played'].sum().reset_index()
+
+    # Ordenar las filas por año
+    horas_por_anio = horas_por_anio.sort_values(by='release_year')
+
+    
+    # Redondear los valores en la lista de acumulación de horas jugadas a 2 decimales
+    horas_por_anio['Hours_Played'] = horas_por_anio['Hours_Played'].round(2)
+
+    # Crear una lista de diccionarios con la acumulación de horas jugadas por año
+    lista_horas_por_anio = [{"Año": anio, "Horas": horas} for anio, horas in zip(horas_por_anio['release_year'], horas_por_anio['Hours_Played'])]
+
+    # Crear el diccionario de retorno
+    resultado = {"Usuario con más horas jugadas para " + genero: usuario_mas_horas, "Horas jugadas": lista_horas_por_anio}
+
+    return resultado
+# Funcion 3 
+@app.get("/usersrecommend/{year}")
+async def UsersRecommend(year: int):
+    # Verifica si el año está en el dataframe
+    if year not in df_final_f3_4['Year_Posted'].unique():
+        return "No se encuentró el año solicitado"
+    
+    # Filtra el dataframe por el año y las recomendaciones
+    df_filtered = df_final_f3_4[(df_final_f3_4['Year_Posted'] == year) & (df_final_f3_4['Recommend'] == True)]
+    
+    # Cuenta las recomendaciones por juego
+    recommend_counts = df_filtered['title'].value_counts()
+    
+    # Obtiene el top 3 de juegos más recomendados
+    top_3 = recommend_counts.nlargest(3)
+    
+    # Crea una lista para almacenar los resultados
+    results = []
+    
+    # Itera sobre el top 3 y añade cada juego a los resultados
+    for i, (game, count) in enumerate(top_3.items(), start=1):
+        results.append({f"Puesto {i}": game})
+    
+    return results
+# Funcion 4
+@app.get("/usersnotrecommend/{year}")
+async def UsersNotRecommend(year: int):
+    # Verifica si el año está en el dataframe
+    if year not in df_final_f3_4['Year_Posted'].unique():
+        return "No se encuentró el año solicitado"
+    
+    # Filtra el dataframe por el año y las no recomendaciones
+    df_filtered = df_final_f3_4[(df_final_f3_4['Year_Posted'] == year) & (df_final_f3_4['Recommend'] == False)]
+    
+    # Cuenta las no recomendaciones por juego
+    not_recommend_counts = df_filtered['title'].value_counts()
+    
+    # Obtiene el top 3 de juegos menos recomendados
+    top_3 = not_recommend_counts.nlargest(3)
+    
+    # Crea una lista para almacenar los resultados
+    results = []
+    
+    # Itera sobre el top 3 y añade cada juego a los resultados
+    for i, (game, count) in enumerate(top_3.items(), start=1):
+        results.append({f"Puesto {i}": game})
+    
+    return results
+
+
+# Funcion 5
+@app.get("/sentiment_analysis/{year}")
+async def sentiment_analysis(year: int):
+
+    # Verifica si el año está en el dataframe
+    if year not in df_final_f5['release_year'].unique():
+        return "No se encuentró el año solicitado"
+    # Filtrar el dataframe por el año dado
+    df_año = df_final_f5[df_final_f5['release_year'] == year]
+    
+     # Contar los valores de 'sentiment_analysis'
+    sentiment_counts = df_año['sentiment_analysis'].value_counts()
+    
+    # Crear un diccionario para almacenar los resultados
+    results = {}
+    
+    # Mapear los valores de 'sentiment_analysis' a sus respectivas categorías
+    for i in sentiment_counts.index:
+        if i == 0:
+            results['Negative'] = int(sentiment_counts[i])
+        elif i == 1:
+            results['Neutral'] = int(sentiment_counts[i])
+        elif i == 2:
+            results['Positive'] = int(sentiment_counts[i])
+    
+    return results
+
+# Ejecutar la aplicación con Uvicorn
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=10000)
